b88be3d42967b5bb5c3007a7407f177fc3a91a88
Jury <jurypassa@gmail.com>
2021-12-13 15:57:46 +0100

    init + inizio Header

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+        <div class="container">
+            <img src="../assets/img/dc-logo.png" alt="logo">
+
+            <nav>
+                <ul>
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>   
+            </nav>
+            
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style>
+
+</style>
